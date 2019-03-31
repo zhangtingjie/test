fe3e388cc5d910a7c8c579cc310629c566f9299c
ztj <1071658154@qq.com>
2019-03-31 12:04:13 +0800

    修改内容

---
+++ src/main/java/FirstDemo.java
@@ -0,0 +1,5 @@
+public class FirstDemo {
+    public static void main(String[] args) {
+        System.out.println("我爱北京天安门s");
+    }
+}
---
+++ src/main/java/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HelloWord2");
+        System.out.println("今天天气怎么样");
+    }
+}
